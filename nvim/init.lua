56454e63ecaeee42bc640c7643f53e555c31b09d
Alexander Petro <alexmpetro@gmail.com>
2023-10-18 13:51:21 -0700

    updated init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,9 @@ require('lazy').setup({
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
+    --below 2 lines are new
+    event = { 'BufReadPre', 'BufNewFile' },
+    autostart = 'true',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
@@ -179,58 +182,61 @@ vim.o.completeopt = 'menuone,noselect'
 vim.wo.number = true
 
 
---getting rid of those blank prefixes
-vim.keymap.set('n', '<leader>w', ':wincmd w<CR>', {desc = ''})
-vim.keymap.set('n', '<leader>d', '<Esc>', {desc = ''})
 
-vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
-vim.keymap.set('i', '<S-Tab>', '<<', { noremap = true, silent = true }) --shift tab works
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) --exit to umbrella directory
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") --move highlighted lines 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") --move highlighted lines 
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndoTree' }) --toggle undotree
-vim.keymap.set('n', 'ff', '<C-w>w', { noremap = true, silent = true }) --into/out of undotree
-vim.keymap.set("n", "J", "<C-d>zz") --half page jumps
-vim.keymap.set('n', 'K', '<C-u>zz', { noremap = true, silent = true }) --half page jumps
+--vim.keymap.set('n', '<leader>w', ':wincmd w<CR>', {desc = ''})
+--vim.keymap.set('n', '<leader>d', '<Esc>', {desc = ''})
+
 vim.keymap.set("n", "H", "B") --segment jumps
 vim.keymap.set('n', 'L', 'W') --segment jumps
 vim.keymap.set("n", "n", "nzzzv") --keeps next in the middle of the page
 vim.keymap.set("n", "N", "Nzzzv") --keeps next in the middgle of the page 
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', '<S-Tab>', '<<', { noremap = true, silent = true }) --shift tab works
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) --move highlighted lines 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) --move highlighted lines 
+vim.keymap.set('n', 'ff', '<C-w>w', { noremap = true, silent = true }) --into/out of undotree
+vim.keymap.set("n", "J", "<C-d>zz", { noremap = true, silent = true }) --half page jumps
+vim.keymap.set('n', 'K', '<C-u>zz', { noremap = true, silent = true }) --half page jumps
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], {desc = 'yank to clipboard'})
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) --exit to umbrella directory
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndoTree' }) --toggle undotree
 vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = 'yank to clipboard'})
 vim.keymap.set("n", "<leader>p", '"+p', { desc = 'paste from clipboard' })
-vim.keymap.set("n", "<leader>c", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = 'search & replace'})
-vim.keymap.set("n", "<leader>l", [[/<C-r><C-w>]], {desc = 'search & replace'})
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
---vim.keymap.set("n", "<leader><leader>", function()
---    vim.cmd("so")
---end)
-
---vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]esume ' })
-vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = 'make executable'})
+vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').oldfiles, { desc = '[ ] Recent Files' })
+vim.keymap.set("n", "<leader>si", [[/<C-r><C-w>]], {desc = '[S]earch [I]nstance'})
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = '[S]earch [R]eplace'})
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch [G]rep' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').grep_string, { desc = '[S]earch [S]tring' })
-vim.keymap.set('n', '<leader>/', function()
+vim.keymap.set('n', '<leader>sb', function()
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
-end, { desc = '[/] Fuzzily search in current buffer' })
+end, { desc = '[S]earch [B]uffer' })
 
+vim.keymap.set('n', '<leader>ga', ':G add ',{ desc = '[G]it [A]dd' })
+vim.keymap.set('n', '<leader>gc', ":G commit -m  ",{ desc = '[G]it [C]ommit' })
+vim.keymap.set('n', '<leader>gp', ':G push ',{ desc = '[G]it [P]ush' })
+vim.keymap.set('n', '<leader>gl', ':G log ',{ desc = '[G]it [L]og' })
+vim.keymap.set('n', '<leader>gs', ':G status',{ desc = '[G]it [S]tatus' })
+vim.keymap.set('n', '<leader>gh', ':G checkout',{ desc = '[G]it c[H]eckout'})
+
+--add maps for split window navigation
 
 require('which-key').register {
---  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
---  ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
---  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+  ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
 }
 
 -- [[ Sets these keymaps after buffer attaches ]]
-local on_attach = function()
---vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, {desc = '[G]oto [D]efinition'})
---vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_references, {desc = '[S]earch [R]eferences'})
-end
+--local on_attach = function()
+--  vim.keymap.set('n', '<leader>sd', require('telescope.builtin').lsp_definitions, {desc = '[S]earch [D]efinition'})
+--  vim.keymap.set('n', '<leader>sr', '<cmd>Telescope lsp_references', {desc = '[S]earch [R]eferences'})
+--end
 
 
 
@@ -279,7 +285,14 @@ end, 0)
 
 --[[ Configure Lsp ]]
 require('mason').setup()
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup({
+  ensure_installed={
+    'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx',
+    'javascript','typescript', 'vimdoc', 'vim', 'bash'
+  }
+})
+
+local lspconfig = require('lspconfig')
 
 local servers = {
 
