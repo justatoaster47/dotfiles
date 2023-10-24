6d184005b60243df80497dba6a9f9848e94ab137
Alexander Petro <alexmpetro@gmail.com>
2023-10-23 23:19:53 -0700

    clean ups

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = ' '
 --Preferences
 vim.o.nu = true
 vim.o.relativenumber = true
+vim.o.number = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
@@ -28,6 +29,7 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 vim.wo.number = true
 vim.o.smartindent = true
+vim.o.autochdir = true
 
 --Keybind Removals below
 --Keybind Sets will be at EOF
@@ -39,7 +41,6 @@ vim.keymap.set({ 'n'}, 'K', '<Nop>', {silent = true})
 vim.keymap.set({ 'n'}, 'H', '<Nop>', {silent = true})
 vim.keymap.set({ 'n', 'v'}, 's', '<Nop>', {silent = true})
 
---vim.keymap.set({ 'n' }, '<gc>', '<Nop>', {silent = true})
 
 
 -- Install package manager
@@ -59,13 +60,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
-  -- Undotree
-  'mbbill/undotree',
-
-  --Editing enclosing characters
-  'tpope/vim-surround',
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -74,8 +68,6 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -130,7 +122,7 @@ require('lazy').setup({
 
         -- don't override the built-in and fugitive keymaps
         local gs = package.loaded.gitsigns
-        vim.keymap.set({ 'n', 'v' }, 'gn', function()
+        vim.keymap.set({ 'n', 'v' }, ']g', function()
           if vim.wo.diff then
             return ']c'
           end
@@ -139,7 +131,7 @@ require('lazy').setup({
           end)
           return '<Ignore>'
         end, { expr = true, buffer = bufnr, desc = '[G]it [N]ext hunk' })
-        vim.keymap.set({ 'n', 'v' }, 'gl', function()
+        vim.keymap.set({ 'n', 'v' }, '[g', function()
           if vim.wo.diff then
             return '[c'
           end
@@ -150,20 +142,6 @@ require('lazy').setup({
         end, { expr = true, buffer = bufnr, desc = '[G]it [L]ast hunk' })
       end,
     },
-  },
-
-  --{
-  --Copilot
-  -- Use ':Copilot setup' to init 
-  --'github/copilot.vim',
-  --lazy = false, 
-  --},
-
-  {
-    'sainnhe/gruvbox-material',
-    config = function()
-      vim.cmd.colorscheme 'gruvbox-material'
-    end,
   },
 
   {
@@ -222,8 +200,34 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-}, {})
 
+
+  --My Additions
+  -- Undotree
+  'mbbill/undotree',
+
+  --Editing enclosing characters
+  'tpope/vim-surround',
+
+  {
+    'sainnhe/gruvbox-material',
+    config = function()
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'lua-language-server',
+        'cpptools',
+        'cpplint',
+      }
+    }
+  },
+
+}, {})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -240,15 +244,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
-    file_ignore_patterns = {
-      --[[
-      '.PNG', '.db', '.qm', '.jpg', '.gif', '.numbers',
-      '.key', '.mp3', '.rar', '.mov', '.HEIC',
-      '.h', '.nam', '.dylib', '.prl', '.wav', '.xtmpat',
-      '.at5p', '.pri', '.JPEG', '.mp4', '.zip', '.repeaks', '.RPP',
-      '.pak', '.at3p', '.at2p',  '.at1p', '.so', '.iso',
-      ]]--
-    },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -262,82 +257,15 @@ require('telescope').setup {
 -- See `:help telescope.builtin`
 pcall(require('telescope').load_extension, 'fzf')
 
-
 -- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
--- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
-    -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
-
     highlight = { enable = true },
     indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-     select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']f'] = '@function.outer',
-          [']c'] = '@class.outer',
-          [']]'] = '@parameter.inner',
-        },
-        --[[goto_next_end = {
-          ['[f'] = '@function.outer',
-          ['[c'] = '@class.outer',
-        },
-        ]]--
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-          ['[c'] = '@class.outer',
-          ['[['] = '@parameter.inner',
-        },
-        --[[
-        goto_previous_end = {
-          [']f'] = '@function.outer',
-          [']c'] = '@class.outer',
-        },
-        --]]
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
   }
 end, 0)
-
-
 
 -- [[ Configure LSP ]]
 local on_attach = function(_, bufnr)
@@ -357,8 +285,8 @@ local on_attach = function(_, bufnr)
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>ss', require('telescope.builtin').lsp_document_symbols, '[S]earch [S]ymbols')
+  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
@@ -436,7 +364,7 @@ cmp.setup {
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     },
   },
   sources = {
@@ -449,8 +377,8 @@ cmp.setup {
 require('which-key').register {
   -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   -- ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  --['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+  --['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 }
 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -463,48 +391,31 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set('i', '<S-Tab>', '<<', { noremap = true, silent = true }) --shift tab works
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) --move highlighted lines 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) --move highlighted lines 
-vim.keymap.set('n', 'ff', '<C-w>w', { noremap = true, silent = true }) --into/out of undotree
+vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true }) --toggle undotree
+vim.keymap.set('n', 'sh', '<C-w>h', { noremap = true, silent = true }) --nvim pane switches
+vim.keymap.set('n', 'sj', '<C-w>j', { noremap = true, silent = true }) --nvim pane switches
+vim.keymap.set('n', 'sk', '<C-w>k', { noremap = true, silent = true }) --nvim pane switches
+vim.keymap.set('n', 'sl', '<C-w>l', { noremap = true, silent = true }) --nvim pane switches
+vim.keymap.set('n', 'ss', '<C-w>w', { noremap = true, silent = true }) --nvim pane switches
 vim.keymap.set("n", "J", "<C-d>zz", { noremap = true, silent = true }) --half page jumps
 vim.keymap.set('n', 'K', '<C-u>zz', { noremap = true, silent = true }) --half page jumps
-
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex, {desc = 'exit to [e]xplore'}) --exit to umbrella directory
 vim.keymap.set('n', '<leader>h', ':cd %:p:h<CR>:pwd<CR>', {noremap = true, silent = true, desc = 'Set path to current directory'})
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], {desc = '[y]ank to clipboard'})
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>dm', vim.diagnostic.open_float, { desc = 'Open [D]iagnostic [M]essage' })
+vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostics [L]ist' })
 vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = '[Y]ank to clipboard'})
 vim.keymap.set("n", "<leader>p", '"+p', { desc = '[p] paste from clipboard' })
 vim.keymap.set("n", "<leader>P", '"0p', { desc = '[P]aste from last yank' })
---vim.keymap.set("n", "<leader>d", '"_d', { desc = '[d]elete into the abyss' })
-
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = 'make executable'})
 vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').oldfiles, { desc = '[ ] Recent Files' })
---vim.keymap.set("n", "<leader>si", [[/<C-r><C-w>]], {desc = '[S]earch [I]nstance'})
---just use asterisk instead of search instance
-vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = '[S]earch [R]eplace'})
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch [G]rep' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').grep_string, { desc = '[S]earch [S]tring' })
-vim.keymap.set('n', '<leader>sb', function()
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[S]earch [B]uffer' })
-
-
--- vim.keymap.set('n', '<leader>ga', ':G add ',{ desc = '[G]it [A]dd' })
--- vim.keymap.set('n', '<leader>gc', ":G commit -m  ",{ desc = '[G]it [C]ommit' })
--- vim.keymap.set('n', '<leader>gp', ':G push ',{ desc = '[G]it [P]ush' })
--- vim.keymap.set('n', '<leader>gl', ':G log ',{ desc = '[G]it [L]og' })
--- vim.keymap.set('n', '<leader>gs', ':G status',{ desc = '[G]it [S]tatus' })
--- vim.keymap.set('n', '<leader>gh', ':G checkout',{ desc = '[G]it c[H]eckout'})
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open [D]iagnostic message' })
-vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostics [L]ist' })
-
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[S]earch [B]uffer' })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
