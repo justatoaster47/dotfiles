648569a0252411391d6db8b7903f853ed4180c25
Alexander Petro <alexmpetro@gmail.com>
2023-12-02 19:22:00 -0800

    standard commit message

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/alexpetro/anaconda3/bin/conda
-    eval /Users/alexpetro/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
-
